439c0d201d299e4e5ca4a103bf5d0d5318e2a696
Salwa Laouni <laounisalwa1937@gmail.com>
2025-01-21 12:15:52 +0100

    Keeping only TP1, TP2, TP3, and TP4

--- TP33/Customer-service/src/main/java/ma/emsi/customerservice/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package ma.emsi.customerservice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-interface CustomerRepository extends JpaRepository<Customer,Long> { }
